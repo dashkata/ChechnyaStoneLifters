c2bcd601f221da0b5522274532d3999862f25eee
Alexander Georgiev <ageorgiev@appolica.com>
2023-03-09 16:43:40 +0200

    Added a gesture detector and started to work on the create request screen

--- lib/presentation/screens/create_request/create_request.dart
+++ lib/presentation/screens/create_request/create_request.dart
@@ -1,5 +1,6 @@
 import 'package:checheneca/presentation/screens/create_request/create_request_vm.dart';
 import 'package:checheneca/presentation/widgets/custom_text_field.dart';
+import 'package:checheneca/presentation/widgets/unfocus_detector.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -8,30 +9,59 @@ class CreateRequestScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Consumer<CreateRequestViewModel>(
-          builder: (context, viewModel, child) => Column(
-            children: [
-              CustomTextField(
-                label: 'Description',
-                controller: viewModel.descriptionController,
-                maxLength: 250,
-              ),
-              CustomTextField(
-                label: 'Starting address',
-                controller: viewModel.descriptionController,
-              ),
-              ElevatedButton(
-                onPressed: () => showModalBottomSheet(
-                  context: context,
-                  builder: (_) => Container(),
+    List<String> skills = ['driver', 'weapon'];
+    List<bool> skillsValue = [true, false];
+    return UnfocusDetector(
+      child: SafeArea(
+        child: Scaffold(
+          body: Consumer<CreateRequestViewModel>(
+            builder: (context, viewModel, child) => Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CustomTextField(
+                  label: 'Description',
+                  controller: viewModel.descriptionController,
+                  maxLength: 250,
                 ),
-                child: const Text(
-                  'Select skills',
+                CustomTextField(
+                  label: 'Starting address',
+                  controller: viewModel.descriptionController,
                 ),
-              ),
-            ],
+                GridView(
+                  shrinkWrap: true,
+                  padding: const EdgeInsets.only(
+                    left: 30,
+                    // top: 100,
+                  ),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 3,
+                  ),
+                  children: List.generate(
+                    viewModel.skillsCheck.length,
+                    (index) => Row(
+                      children: [
+                        Checkbox(
+                          value: viewModel.skillsCheck.values.elementAt(index),
+                          onChanged: (value) => viewModel.changeCheckbox(
+                            index,
+                            value!,
+                          ),
+                        ),
+                        Text(
+                          viewModel.skillsCheck.keys.elementAt(index),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () async => await viewModel.submitRequest(),
+                  child: const Text(
+                    'Submit request',
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/presentation/screens/create_request/create_request_vm.dart
+++ lib/presentation/screens/create_request/create_request_vm.dart
@@ -2,6 +2,26 @@ import 'package:flutter/cupertino.dart';
 
 class CreateRequestViewModel extends ChangeNotifier {
   final TextEditingController descriptionController = TextEditingController();
-  final TextEditingController startingAddressController = TextEditingController();
-  Future<void> init() async {}
+  final TextEditingController startingAddressController =
+      TextEditingController();
+  final List<bool> checkBoxList = [];
+  List<String> skills = ['driver', 'weapon', ' kyr'];
+  Map<String, bool> skillsCheck = {};
+
+  Future<void> init() async {
+    for (final skill in skills) {
+      skillsCheck[skill] = false;
+    }
+  }
+
+  void changeCheckbox(int index, bool newValue) {
+    skillsCheck.update(skillsCheck.keys.elementAt(index), (value) => newValue);
+    notifyListeners();
+  }
+
+  Future<void> submitRequest() async {
+
+    // skillsCheck.map((key, value) => )
+
+  }
 }
